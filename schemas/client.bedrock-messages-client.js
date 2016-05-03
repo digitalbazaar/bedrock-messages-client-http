9231b3fe461b580cc1ca7e722322770a45d5b6f1
Bilal Shahid <bshahid@digitalbazaar.com>
2016-05-03 17:18:47 -0400

    Added validation and authentication

---
+++ schemas/client.bedrock-messages-client.js
@@ -0,0 +1,19 @@
+var schemas = require('bedrock-validation').schemas;
+var constants = require('bedrock').config.constants;
+
+var client = {
+  type: 'object',
+  properties: {
+    id: schemas.identifier(),
+    endpoint: schemas.url(),
+    label: schemas.label(),
+    publicKeyId: {required: true, type: 'string'},
+    interval: {required: true, type: 'integer', minimum: 1},
+    strictSSL: {type: 'boolean', required: true},
+    changeKey: {type: 'boolean', required: true}
+  },
+};
+
+module.exports.client = function() {
+  return client;
+};
